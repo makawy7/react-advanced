909b13a2930046f3d9c593fb55801fd0a290afc3
abdallah <abdallahelmekawy6@gmail.com>
2023-02-05 22:32:50 +0200

    useEffect dependency list

--- src/components/UseEffectBasics.js
+++ src/components/UseEffectBasics.js
@@ -2,16 +2,33 @@ import { useEffect, useState } from "react";
 
 function UseEffectBasics() {
   const [value, setValue] = useState(0);
-  //   useEffect runs after every re-render
+  const [another, setAnother] = useState(0);
+  //  useEffect runs after every re-render (depending on it's dependency list).
+  // if dependency list was not passed, it will re-render with any change.
   useEffect(() => {
-    document.title = `New messages ${value}`;
-  });
+    if (value > 5) {
+      document.title = `New messages ${value}`;
+    }
+  }, [value]);
+  // runs only on inital render and as a side effect of the dependency list.
+  useEffect(() => {
+    console.log('runs only as a side effect of "another" changings');
+  }, [another]);
+
+  // if the dependency list is empty, it will run once after the initial render.
+  useEffect(() => {
+    console.log("Runs once after the initial render");
+  }, []);
 
   return (
     <>
-      <h1>{value}</h1>
+      <h1>value: {value}</h1>
+      <h1>another: {another}</h1>
       <button className="btn" onClick={() => setValue(value + 1)}>
-        Click me
+        change value
+      </button>
+      <button className="btn" onClick={() => setAnother(another + 1)}>
+        change another
       </button>
     </>
   );
