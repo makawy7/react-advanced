8a7106ed665946b3986f45f59675b46a15ba8606
abdallah <abdallahelmekawy6@gmail.com>
2023-02-06 01:58:57 +0200

    ternary

--- src/components/ShortCircuit.js
+++ src/components/ShortCircuit.js
@@ -2,13 +2,26 @@ import { useState } from "react";
 
 function ShortCircuit() {
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
   return (
     <>
-      <h1>first: {firstValue}</h1>
-      <h1>second: {secondValue}</h1>
+      {/* <h1>first: {firstValue}</h1>
+      <h1>second: {secondValue}</h1> */}
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      <h1 style={{ marginTop: "10px" }}>{isError && "Error"}</h1>
+      {isError ? (
+        <p>there's an error ...</p>
+      ) : (
+        <div>
+          <h2>there's no error</h2>
+        </div>
+      )}
     </>
   );
 }
