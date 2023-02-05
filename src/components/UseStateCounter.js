2c05515daccbc779f886ae315ab5332570c751a7
abdallah <abdallahelmekawy6@gmail.com>
2023-02-05 21:11:21 +0200

    setting useState value with function return

--- src/components/UseStateCounter.js
+++ src/components/UseStateCounter.js
@@ -2,19 +2,35 @@ import { useState } from "react";
 
 function UseStateCounter() {
   const [value, setValue] = useState(0);
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+    //   setValue(value + 1);
+      setValue((prevState) => prevState + 1);
+    }, 2000);
+  };
   return (
     <>
-      <h2>Simple Counter</h2>
-      <h2>{value}</h2>
-      <button onClick={() => setValue(value - 1)} className="btn">
-        Decrease
-      </button>
-      <button onClick={() => setValue(0)} className="btn">
-        Reset
-      </button>
-      <button onClick={() => setValue(value + 1)} className="btn">
-        Increase
-      </button>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Simple Counter</h2>
+        <h2>{value}</h2>
+        <button onClick={() => setValue(value - 1)} className="btn">
+          Decrease
+        </button>
+        <button onClick={() => setValue(0)} className="btn">
+          Reset
+        </button>
+        <button onClick={() => setValue(value + 1)} className="btn">
+          Increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h2>{value}</h2>
+        <button onClick={complexIncrease} className="btn">
+          Increase later
+        </button>
+      </section>
     </>
   );
 }
