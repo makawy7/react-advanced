6f9f13796a7bea1e61bf040f5cb73604efc2611f
abdallah <abdallahelmekawy6@gmail.com>
2023-02-08 12:59:39 +0200

    using useCallback with useFetch

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const getProducts = async () => {
-      const response = await fetch(url);
-      // console.log(response.status);
-      const myProducts = await response.json();
-      setProducts(myProducts);
-      setLoading(false);
-    };
-    getProducts();
+
+  const getProducts = useCallback(async () => {
+    const response = await fetch(url);
+    // console.log(response.status);
+    const myProducts = await response.json();
+    setProducts(myProducts);
+    setLoading(false);
   }, [url]);
+
+  useEffect(() => {
+    getProducts();
+  }, [url, getProducts]);
+  
   return { loading, products };
 };
